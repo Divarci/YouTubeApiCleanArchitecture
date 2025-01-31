56dc5d9f9c45840aeb9510dafaa1f54d1007cfc4
Hasan Divarci <hasandivarciuk@hotmail.com>
2025-01-31 22:38:50 +0000

    Functional Test

---
+++ test/YouTubeApiCleanArchitecture.API.FunctionalTests/Identity/AuthTests.cs
@@ -0,0 +1,66 @@
+﻿using FluentAssertions;
+using System.Net;
+using System.Net.Http.Json;
+using YouTubeApiCleanArchitecture.API.FunctionalTests.Infrastructure;
+using YouTubeApiCleanArchitecture.Application.Features.Identity.AppUsers.Commands.LoginUser;
+using YouTubeApiCleanArchitecture.Domain.Abstraction.ResultPattern;
+using YouTubeApiCleanArchitecture.Domain.Entities.Identity.Users.DTOs;
+
+namespace YouTubeApiCleanArchitecture.API.FunctionalTests.Identity;
+public class AuthTests : BaseFunctionalTest
+{
+    public AuthTests(FunctionalTestWebAppFactory factory) : base(factory)
+    {
+    }
+
+    [Fact]
+    public async Task Register_ShouldReturn_Success()
+    {
+        //arrange
+
+        var request = new RegisterUserDto
+        {
+            Email = "test2@test.com",
+            Password = "Password12**",
+            ConfirmPassword = "Password12**",
+            Fullname = "Test2 Person"
+        };
+
+        //act
+
+        var response = await HttpClient.PostAsJsonAsync(
+            "api/v1/auth/register",
+            request);
+
+        //assert
+
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+    }
+
+    [Fact]
+    public async Task Login_ShouldReturn_Success()
+    {
+        //arrange
+
+        var request = new LoginUserDto
+        {
+            Email = "test2@test.com",
+            Password = "Password12**"           
+        };
+
+        //act
+
+        var response = await HttpClient.PostAsJsonAsync(
+            "api/v1/auth/login",
+            request);
+
+        var content = await response.Content
+            .ReadFromJsonAsync<Result<LoginUserResponse>>();
+
+        //assert
+
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+        content.Data.AccessToken.Should().NotBeNull();
+    }
+}
---
+++ test/YouTubeApiCleanArchitecture.API.FunctionalTests/Infrastructure/BaseFunctionalTest.cs
@@ -0,0 +1,43 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Authentication.BearerToken;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System.Net.Http.Json;
+using YouTubeApiCleanArchitecture.Domain.Entities.Identity.Users.DTOs;
+using YouTubeApiCleanArchitecture.Infrastructure;
+
+namespace YouTubeApiCleanArchitecture.API.FunctionalTests.Infrastructure;
+public abstract class BaseFunctionalTest : IClassFixture<FunctionalTestWebAppFactory>
+{
+    protected readonly HttpClient HttpClient;
+    private readonly IServiceScope _scope;
+    protected readonly AppDbContext DbContext;
+
+
+    protected BaseFunctionalTest(FunctionalTestWebAppFactory factory)
+    {
+        HttpClient = factory.CreateClient();
+
+        _scope = factory.Services.CreateScope();
+
+        DbContext = _scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+        DbContext.Database.Migrate();
+    }
+
+    protected async Task<string> GetAccessToken()
+    {
+        HttpResponseMessage loginResponse = await HttpClient.PostAsJsonAsync(
+            "api/v1/auth/login",
+            new LoginUserDto
+            {
+                Email = "test@test.com",
+                Password = "Password12**"
+            });
+
+        var accessTokenResponse = await loginResponse.Content
+            .ReadFromJsonAsync<AccessTokenResponse>();
+
+        return accessTokenResponse!.AccessToken;
+    }
+}
